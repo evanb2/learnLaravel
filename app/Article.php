943443a7372523e0391f9fe658654b54b1c00be7
Evan Butler <evanb2@mac.com>
2015-07-24 00:34:00 -0700

    Setup Article model with fillable array; add Eloquent notes

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    //Sets which fields have permission to use the App\Article::create() method
+        //Don't put in things like $id or $password
+    protected $fillable = [
+        'title',
+        'body',
+        'published_at'
+    ];
 }
