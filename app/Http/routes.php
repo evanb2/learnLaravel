b2523503e66dbf3606642797df0ca60dd4b7e950
Evan Butler <evanb2@mac.com>
2015-07-25 12:31:42 -0700

    Finish Forms lesson

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,4 +21,6 @@ Route::get('contact', function () {
 });
 
 Route::get('articles', 'ArticlesController@index');
+Route::get('articles/create', 'ArticlesController@create');
 Route::get('articles/{id}', 'ArticlesController@show');
+Route::post('articles', 'ArticlesController@store');
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Master Page</title>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"> -->
 </head>
 <body>
     <div class="container">
