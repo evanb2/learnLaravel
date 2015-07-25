664d71a0619248ec041256a616d819c1424ec4e5
Evan Butler <evanb2@mac.com>
2015-07-25 16:09:13 -0700

    Add doc blocks

--- app/Article.php
+++ app/Article.php
@@ -7,26 +7,53 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //Sets which fields have permission to use the App\Article::create() method
-        //Don't put in things like $id or $password
+    //
+        //
+    /**
+     * Fillable fields for an Article.
+     * Sets which fields have permission to use the App\Article::create() method.
+     * Don't put in things like $id or $password.
+     *
+     * @var array
+     */
     protected $fillable = [
         'title',
         'body',
         'published_at'
     ];
 
+    /**
+     * Additional fields to treat as Carbon instances.
+     *
+     * @var array
+     */
     protected $dates = ['published_at'];
 
+    /**
+     * Scope queries to articles that have been published.
+     *
+     * @param $query
+     */
     public function scopePublished($query)
     {
         $query->where('published_at', '<=', Carbon::now());
     }
 
+    /**
+     * Scope queries to articles that have NOT been published.
+     *
+     * @param $query
+     */
     public function scopeUnpublished($query)
     {
         $query->where('published_at', '>=', Carbon::now());
     }
 
+    /**
+     * Set the published_at attribute.
+     *
+     * @param $date
+     */
     public function setPublishedAtAttribute($date)
     {
         $this->attributes['published_at'] = Carbon::parse($date);
